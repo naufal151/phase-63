c7cbc1972309b29d72470339ac1ee652498e1257
AlbiruniWibawa <albiruni4141@gmail.com>
2021-08-02 11:58:19 +0700

    ABOUT US FINISHED

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -1,4 +1,4 @@
-var slideIndex = 1;
+var slideIndex = 4;
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -13,14 +13,17 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
+  var text = document.getElementsByClassName("meaning");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+      slides[i].style.display = "none";
+      text[i].style.display = "none";  
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";  
+  text[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
